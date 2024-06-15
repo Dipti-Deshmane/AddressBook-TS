c7f0af0b5b38363a98a1bc7a3fb7c722e41fa40e
Dipti-Deshmane <itsmedipu17@gmail.com>
2024-06-15 11:57:37 +0530

    UC1 added

--- AddressBook.ts
+++ AddressBook.ts
@@ -1,1 +1,9 @@
-console.log("Welcome to address book");
+class Contact{
+    private firstName : string;
+    private lastName : string;
+    private address :string;
+    private city : string;
+    private zip : number;
+    private contact :number;
+    private email : string | number;
+}
